90fcd9a9a627e3065311296485d310c6dda2ecad
Kirill Korotkov <kkorotkov@owner.one>
2024-02-22 13:22:19 +0100

    feat: first graphql request

---
+++ src/hooks/use_query.rs
@@ -0,0 +1,52 @@
+use yew::{hook, use_effect_with, use_state, UseStateHandle};
+
+#[hook]
+pub fn use_query<T: 'static + serde::de::DeserializeOwned>(
+    body: &str,
+) -> UseStateHandle<Option<T>> {
+    let state: UseStateHandle<Option<T>> = use_state(|| None::<T>);
+    let controller = web_sys::AbortController::new().unwrap();
+    let signal = controller.signal();
+
+    let request_builder = match gloo_net::http::Request::post("/api/v0/graphql")
+        .abort_signal(Some(&signal))
+        .header("Content-Type", "application/json")
+        .body(body)
+    {
+        Ok(request_builder) => request_builder,
+        Err(err) => {
+            web_sys::console::error_1(&err.to_string().into());
+            return state;
+        }
+    };
+
+    {
+        let state = state.clone();
+
+        use_effect_with((), move |_| {
+            wasm_bindgen_futures::spawn_local(async move {
+                let response = match request_builder.send().await {
+                    Ok(response) => response,
+                    Err(err) => {
+                        web_sys::console::error_1(&err.to_string().into());
+                        return;
+                    }
+                };
+
+                let response: T = match response.json().await {
+                    Ok(response_json) => response_json,
+                    Err(_) => {
+                        web_sys::console::error_1(&"Failed to parse response".into());
+                        return;
+                    }
+                };
+
+                state.set(Some(response));
+            });
+
+            move || controller.abort()
+        });
+    }
+
+    state
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,55 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+mod hooks;
+
+use hooks::use_query::use_query;
+
+#[derive(serde::Deserialize)]
+struct LoginResponse {
+    login: String,
+}
+
+#[function_component]
+fn Home() -> Html {
+    let state: UseStateHandle<Option<LoginResponse>> =
+        use_query::<LoginResponse>(r#"{"query":"{ login }"}"#);
+
+    let token = state
+        .as_ref()
+        .map(|response| response.login.as_str())
+        .unwrap_or("loading...");
+
+    html! {
+        <>
+            <h1>{ "Home" }</h1>
+            <p>{ token }</p>
+        </>
+    }
+}
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/404")]
+    #[not_found]
+    NotFound,
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! { <Home /> },
+        Route::NotFound => html! { <h1>{ "Not Found" }</h1> },
+    }
+}
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
+       <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
     }
 }
 
